b143d347c7e72ece4e2befd4940b4a00fe92e46f
Vipul K Mishra <Vipul.Mishra@gdit.com>
2024-08-09 05:03:13 -0400

    scan and delay:RxJS

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { delay,filter } from 'rxjs';
+import { delay,filter,scan } from 'rxjs';
 import { FormGroup, FormControl } from '@angular/forms';
 import { MathValidators } from '../math-validators';
 
@@ -11,6 +11,9 @@ import { MathValidators } from '../math-validators';
 })
 export class EquationComponent {
 
+  solutionPerSeconds = 0.0;
+
+
   mathForm = new FormGroup({
     a: new FormControl(this.randomNumbers()),
     b: new FormControl(this.randomNumbers()),
@@ -19,16 +22,29 @@ export class EquationComponent {
   }, [MathValidators.addition ('answer','a','b')]);
   constructor() {
 
+    //const startTime = new Date().getTime();
+    //let numberofProbSolved = 0;
+
+
     this.mathForm.statusChanges.pipe(
      filter(value=>value==='VALID'),
-      delay(200)
-    ).subscribe(value => {
+      delay(200),
+      scan(acc => { 
+
+        return {numberSolved1:acc.numberSolved1+1,timeS:acc.timeS}
+      }, {numberSolved1:0,timeS:new Date()})
+    //).subscribe(value => {
+    ).subscribe(({numberSolved1,timeS }) => {
+     // numberofProbSolved++;
+      //this.solutionPerSeconds = (new Date().getTime()-startTime) / numberofProbSolved/1000;
+      this.solutionPerSeconds = (new Date().getTime()-timeS.getTime()) / numberSolved1/1000;
       
       // if (value === 'INVALID') {
       //   return;
       // }
 
       this.mathForm.patchValue({
+        a:this.randomNumbers(),
         b: this.randomNumbers(),
         answer:''
 
